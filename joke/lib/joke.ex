b8dee3e418bed6756531c97a59a89144802e6cf2
KirillSattkevihc <hhehe4657@gmail.com>
2022-01-04 11:55:27 +0300

    joke library

--- joke/lib/joke.ex
+++ joke/lib/joke.ex
@@ -1,25 +1,12 @@
 defmodule Joke do
   def get_joke() do
-    case HTTPoison.get("https://github.com/UltiRequiem/joke") do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-    url =
-      body
-      |> Floki.find("a")
-      |> Floki.attribute("href")
-      |> Enum.find( fn x -> x == "https://joke.deno.dev" end)
-      |> to_string()
-    case HTTPoison.get(url) do
+    case HTTPoison.get("https://joke.deno.dev") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-         print_joke(body)
+        print_joke(body)
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "joke_api Not found :("
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
-    end
-    {:ok, %HTTPoison.Response{status_code: 404}} ->
-      IO.puts "GitHub not found :("
-    {:error, %HTTPoison.Error{reason: reason}} ->
-      IO.inspect reason
     end
   end
 
